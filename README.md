044cf35ca34a3b75077537d8d0bfb8035726e14a
Jean Rafael Lourenço <22452663+jeanrafaellourenco@users.noreply.github.com>
2022-11-28 15:30:30 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Gerador de Certificado
+Gerador de certificado em PDF 
+
+## Configurações de impressão
+
+- Utilize o tamanho do papel **Tabloid** e o Layout como **Paisagem**.  
+
+- O nome pode ser inserido pelo botão `INSERIR NOME` ou via parâmetro na url:  
+ `..../index.html?nome=Gulherme%20Briggs`  
+
+![impressao](./images/impressao.png)  
+#
+## Todo
+
+- [ ] API de geração de certificado  
+  - [ ] Implementar consulta e validação de certificado
+- [ ] Melhorias no layout de impressão
+
+### In Progress
+
+- [ ]   
+
+### Done ✓
+
+- [] 
---
+++ js/script.js
@@ -0,0 +1,45 @@
+var now = new Date;
+var month = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro");
+var data = '<b>São Paulo</b>, ' + now.getDate() + ' de ' + month[now.getMonth()]  + ' de ' + now.getFullYear()
+var pessoa = 'Seu Nome Aqui';
+var marcadagua ="<br><br>CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO CERTIFICADO \
+"
+var button = document.querySelector('button');
+
+document.getElementById("pessoa").innerHTML = pessoa;
+document.getElementById("data").innerHTML = data;
+document.getElementById("marcadagua").innerHTML = marcadagua;
+
+button.onclick = function () {
+    var pessoa = prompt('Digite o nome da pessoa: ');
+    document.getElementById("pessoa").innerHTML = pessoa;
+    var titulo = pessoa.replace(/ /g, "_");
+    document.getElementById("titulo").innerHTML = titulo;
+}
+// Recebendo nome via parâmetro da url
+const urlParams = new URLSearchParams(window.location.search);
+var url_pessoa = urlParams.get('nome')
+
+if (url_pessoa) {
+    document.getElementById("pessoa").innerHTML = url_pessoa;
+    var titulo = url_pessoa.replace(/ /g, "_");
+    document.getElementById("titulo").innerHTML = titulo;
+}
